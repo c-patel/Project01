cf3ffca71564537ebb1a8505e8321d2b8f253095
c-patel <c.p@outlook.in>
2017-01-22 16:23:42 +0530

    Version 01 Update @ 22 JAN 2017

--- app/src/main/java/com/cpsoft/testinggit01/MainActivity.java
+++ app/src/main/java/com/cpsoft/testinggit01/MainActivity.java
@@ -2,12 +2,15 @@ package com.cpsoft.testinggit01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG="GiTesting";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: Update 01");
     }
 }
